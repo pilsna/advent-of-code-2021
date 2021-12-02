302ed0e02532de590c844d6b62ebb1dd53b72ddd
Nils Rydh <nils.rydh@gmail.com>
2021-12-02 17:47:06 +0100

    solved part two

--- december-02/rydh/Dive.java
+++ december-02/rydh/Dive.java
@@ -1,23 +1,46 @@
 package rydh;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Dive {
     private static final String CSV_DELIMITER = " ";
 
     public static void main(String args[]) {
-        Map<String, Integer> map = loadFromFile("./december-02/rydh/Dive.txt");
-        System.out.println((map.get("down") - map.get("up")) * map.get("forward"));
-
         List<List<String>> dataList = loadFromCsv("./december-02/rydh/Dive.txt");
+        partOneCalculation(dataList);
+        partTwoCalculation(dataList);
+    }
+
+    private static void partTwoCalculation(List<List<String>> dataList) {
+        int forward = 0;
+        int up = 0;
+        int down = 0;
+        double aim = 0;
+        for (List<String> list : dataList) {
+            switch (list.get(0)) {
+                case "forward":
+                    forward += Integer.parseInt(list.get(1));
+                    down += aim * Integer.parseInt(list.get(1));
+                    break;
+                case "up":
+                    aim -= Integer.parseInt(list.get(1));
+                    break;
+                case "down":
+                    aim += Integer.parseInt(list.get(1));
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("partTwoCalculation " + forward * (down - up));
+    }
+
+    private static void partOneCalculation(List<List<String>> dataList) {
         int forward = 0;
         int up = 0;
         int down = 0;
@@ -36,15 +59,7 @@ public class Dive {
                     break;
             }
         }
-        System.out.println(forward * (down - up));
-    }
-
-    public static String mapToString(Map<String, Integer> map) {
-        StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            sb.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
-        }
-        return sb.toString();
+        System.out.println("partOneCalculation " + forward * (down - up));
     }
 
     public static List<List<String>> loadFromCsv(String fileName) {
@@ -61,28 +76,5 @@ public class Dive {
             e.printStackTrace();
         }
         return records;
-    }
-
-    public static Map<String, Integer> loadFromFile(String fileName) {
-        File file = new File(fileName);
-        BufferedReader reader;
-        Map<String, Integer> map = new HashMap<>();
-        map.put("forward", 0);
-        map.put("up", 0);
-        map.put("down", 0);
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] columns = line.split(" ");
-                int sum = map.get(columns[0]);
-                sum = Integer.parseInt(columns[1]) + sum;
-                map.put(columns[0], sum);
-            }
-            reader.close();
-        } catch (NumberFormatException | IOException e) {
-            e.printStackTrace();
-        }
-        return map;
     }
 }
